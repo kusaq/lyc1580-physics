6e6289a49af61710b9bbed8f12b46d5724e63101
Kusaq <dzhamal@me.com>
2018-10-29 21:52:02 +0300

    Removed some rubbish in output

--- fp5.py
+++ fp5.py
@@ -1,8 +1,4 @@
-print()
-print()
 print('#### POWERS THEN AMPERAGES ####')
-print()
-print()
 
 data = [float(x) for x in input().split()]
 amperages = [float(x) for x in input().split()]
@@ -12,14 +8,9 @@ r_mini = 0.073
 r_big = 0.177
 n = 600.0
 
+print()
 for i in range(1, len(data), +2):
     powers += [float(data[i-1] + data[i])]
 
-print('-'*20)
-
 for i in range(len(powers)):
     print(i,'| P =', round((amperages[i]*3.14*((r_big)**2)*n), 3), 'AND M =', round((powers[i]*r_mini), 3))
-
-print('-'*20)
-
-print()
